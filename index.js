23d44315cf6d43cbac59a1d187be78dddc4ce4dc
Mustafa <mustafa_9611@hotmail.com>
2023-01-07 16:07:14 +0100

    api/shorturl should get JSON respond with orig url

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// api/shorturl should get a JSON respond with original_url and short_url properties. When you visit api/shorturl/<short_url>, you will be redirected to the original URL and if the original URL doesn't exist, you will get a JSON response with error: 'invalid URL'
+app.post('/api/shorturl', function(req, res) {
+  res.json({ original_url: 'https://www.google.com', short_url: 1 });
+});
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+  if (req.params.short_url === '1') {
+    res.redirect('https://www.google.com');
+  } else {
+    res.json({ error: 'invalid URL' });
+  }
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
