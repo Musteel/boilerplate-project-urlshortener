9aebd75764d1f8521e72b4dacee4cffe5f5d9db4
Mustafa <mustafa_9611@hotmail.com>
2023-01-07 17:56:04 +0100

    fix code

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const bodyParser = require('body-parser');
-
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 
 app.use(cors());
-
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -24,14 +21,30 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// api/shorturl and get a JSON response with original_url and short_url properties like this: {"original_url","https://freeCodeCamp.org","short_url":1}
-app.post('/api/shorturl', function(req, res) {
-  res.json({ original_url: req.body.url, short_url: 1 });
+const original_url = [];
+const short_url = [];
+// api/shorturl and get a JSON response with original_url and short_url properties like this: {"original_url","https://freeCodeCamp.org","short_url":1} and if you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain an error like this: {"error":"invalid URL"}. HINT: to be sure that the submitted url points to a valid site you can use the function dns.lookup(host, cb) from the dns core module.
+
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url;
+  const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+  if (urlRegex.test(url)) {
+    original_url.push(url);
+    short_url.push(original_url.length);
+    res.json({ original_url: url, short_url: original_url.length });
+  } else {
+    res.json({ error: 'invalid URL' });
+  }
 });
 
-// When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
-app.get('/api/shorturl/:short_url', function(req, res) {
-  res.redirect('https://freeCodeCamp.org');
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const short = req.params.short_url;
+  const index = short_url.indexOf(Number(short));
+  if (index !== -1) {
+    res.redirect (original_url[index]); 
+  } else {
+    res.json({ error: 'invalid URL' });
+  }
 });
 
 
