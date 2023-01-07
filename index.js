d06a893beed82620c334b171ae3ebba1c7aaaf5e
Mustafa <mustafa_9611@hotmail.com>
2023-01-07 16:35:25 +0100

    fix code

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 const validUrl = require('valid-url');
-const shortid = require('shortid');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +19,16 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+// api/shorturl should get a JSON respond with original_url and short_url properties. When you visit api/shorturl/<short_url>, you will be redirected to the original URL and if the original URL doesn't exist, you will get a JSON response with error: 'invalid URL'
+
+// Regular expression to check if a URL is in the format "http://www.example.com"
+const urlRegex = /^https?:\/\/www\.example\.com$/;
+
+// Dictionary to store short URLs and their corresponding original URLs
 const urlMapping = {};
 
 app.post('/api/shorturl', (req, res) => {
@@ -24,7 +36,7 @@ app.post('/api/shorturl', (req, res) => {
   const originalUrl = req.body.url;
 
   // Check if the URL is valid
-  if (!validUrl.isUri(originalUrl)) {
+  if (!urlRegex.test(originalUrl)) {
     return res.json({ error: 'invalid url' });
   }
 
